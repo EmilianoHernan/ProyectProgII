bda58e2faed52009301bc2e63c425cf5e56e532a
EmilianoHernan <emilianohernan92@gmail.com>
2023-07-26 15:41:38 -0300

    depuracion activada y funcion sencilla

--- app.py
+++ app.py
@@ -2,5 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+#Enrutamientos, Ruta raiz "/"
+@app.route('/') 
+def index():
+    return "hola mundo!"
+
+#Si estamos en el archivo main de nuestra aplicacion, la ejecutamos
+#Activamos la depuracion
 if __name__== "__main__":
-    app.run()
+    app.run(debug=True)
