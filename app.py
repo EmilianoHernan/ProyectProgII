04f4686833bc5dd9b1faa36be39256bfa815b562
EmilianoHernan <emilianohernan92@gmail.com>
2023-07-28 03:01:31 -0300

    corrigiendo cosas y agregando mas enrutamientos y haciendo funcionar el logueo

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, jsonify, request, session, redirect
+from flask import Flask, render_template, jsonify, request, session, redirect, url_for
 import json
 
 app = Flask(__name__)
+app.secret_key='LaCoMuNAOSOS'
 
 #Enrutamientos, Ruta raiz "/"
 @app.route('/') 
@@ -10,27 +11,35 @@ def index():
 
 @app.route("/login", methods=['POST', 'GET'])
 def login():
+        usuarioIngresado = None # Asignandole valor predeterminado a la variable, sino rompe el codigo
         #Abro mi archivo json y lo paso a un objeto de python en la variable users_registrados
         with open("usuarios.json", encoding="utf-8") as file:
               registrados =json.load(file)
             
         if request.method == "POST" :
-              usuarioIngresado = request.form["usuario"]
+              usuarioIngresado = request.form["nombre"]
               contraseniaIngresada = request.form["contrasenia"]
 
         #verifico si el usuario existe o no
         for usuario in registrados:
-              if usuario ["usuario"] == usuarioIngresado and contrasenia ["contrasenia"] == contraseniaIngresada
-              session ["usuario"] = usuarioIngresado
-              return redirect
+              if usuario["nombre"] == usuarioIngresado and usuario["contrasenia"] == contraseniaIngresada:      
+                session ["nombre"] = usuarioIngresado
+                return redirect (url_for("main"))
         else:
               mensajeError = "Usuario o contraseña incorrectas, vuelve a intentarlo"
-              return render_template('ingreso.html' error = mensajeError)
-
-        return render_template("ingreso.html")
+              return render_template ('ingreso.html', error = mensajeError)
 
 
+@app.route("/main")
+def main():
+    if "nombre" in session:
+        return render_template("main.html")
+    else:
+        return redirect(url_for("login"))
 
+@app.route("/ingreso")
+def ingresar():
+      return render_template("ingreso.html")
 
 
 @app.route("/ultimaspelis")
@@ -40,7 +49,7 @@ def ultimas_peliculas():
             ultimas_10_peliculas = peliculas[-10:]
             return jsonify(ultimas_10_peliculas)
 
-@app.route("/register")
+@app.route("/registro")
 def registro():
       return render_template("registro.html")       
 
